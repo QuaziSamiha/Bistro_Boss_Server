8990a5b33a3cc26221c89d1d510c2238b05fb073
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-08 21:15:46 -0800

    delete item performed successfully

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
@@ -42,19 +42,27 @@ async function run() {
 
     // cart collection
     app.get("/carts", async (req, res) => {
-      const email = res.query.email;
-      console.log(email);
+      const email = req.query.email;
+      // console.log(email);
       if (!email) {
         res.send([]);
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
+      res.send(result);
     });
 
     app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -81,7 +89,7 @@ app.listen(port, () => {
  * users : userCollection
  * app.get('/users') ----- will get all users -----
  * app.get('/users:id')  ------------- for getting a particular user ---------
- * app.post('/users') ----- create a new user 
+ * app.post('/users') ----- create a new user
  * app.patch('/users:id') ------ for updating a particular user ----
  * app.put('/users:id')
  * app.delete('/users:id')
