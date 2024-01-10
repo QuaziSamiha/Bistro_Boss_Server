ad9956a2c4f5ffaabdd90c2a00b0f8769a0ba24d
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-09 18:48:30 -0800

    advance jwt task remained for later

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ app.use(express.json());
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res.status(401);
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 };
 
 app.post("/jwt", async (req, res) => {
@@ -94,16 +107,34 @@ async function run() {
     });
 
     // cart collection
-    app.get("/carts", async (req, res) => {
+    app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
       // console.log(email);
       if (!email) {
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
+    // app.get("/carts", async (req, res) => {
+    //   const email = req.query.email;
+    //   // console.log(email);
+    //   if (!email) {
+    //     res.send([]);
+    //   }
+    //   const query = { email: email };
+    //   const result = await cartCollection.find(query).toArray();
+    //   res.send(result);
+    // });
 
     app.post("/carts", async (req, res) => {
       const item = req.body;
