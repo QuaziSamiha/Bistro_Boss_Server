e5ea56b08e6ab6b5eaf987db7e71655b988fa01f
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-09 17:32:00 -0800

    working on JWT

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const app = express();
 const port = process.env.PORT || 5000;
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401);
+  }
+};
+
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+  res.send({ token });
+});
+
 // some thing
 app.get("/", (req, res) => {
   res.send("bosss is sitting");
@@ -48,6 +66,18 @@ async function run() {
         return res.send({ message: "user already exists" });
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: `admin`,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
